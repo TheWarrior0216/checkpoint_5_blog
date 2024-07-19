274af9dce838ace452e43a44178627ca2fe9285c
Seth Baxter <TheWarrior0216@gmail.com>
2024-07-19 10:48:29 -0600

    Beggining of Checkpoint

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted } from "vue";
+import { postsService } from "../services/PostsService.js";
+
+onMounted(()=> getPosts())
+
+async function getPosts(){
+  await postsService.getPosts()
+}
+
+</script>
+
+<template>
+  <h1>Hello World!</h1>
+</template>
+
+<style scoped lang="scss">
+
+
+</style>
+
